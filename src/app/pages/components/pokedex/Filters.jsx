e5193e11d635d12a30a95006f861762d24329cf3
Matias Diaz <mdiaz@alquilando.com>
2022-07-27 18:17:56 -0300

    Change files and folder architecture

--- src/app/pages/components/pokedex/Filters.jsx
+++ src/app/pages/components/pokedex/Filters.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
-import { CheckBox, Input, Select } from '../../../../shared/components/Inputs/Inputs';
-import { getPokeProviders } from '../../../../shared/providers/Api';
+import { CheckBox, Input, Select } from '../../../shared/components/Inputs';
+import { getPokeProviders } from '../../../shared/providers/Api';
 
 function Filters({submit}) {
 
